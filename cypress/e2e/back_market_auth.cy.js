07ae7c56d6822300d415ddc39d16a77348de733a
fturello <92311869+fturello@users.noreply.github.com>
2023-08-01 12:34:15 +0200

    updated

--- cypress/e2e/back_market_auth.cy.js
+++ cypress/e2e/back_market_auth.cy.js
@@ -1,36 +1,63 @@
-describe("Tests d'inscription et d'authentification", () => {
-	const validEmail = "test@example.com";
-	const validPassword = "Testpassword123!";
+function generateUniqueEmail() {
+	var timestamp = Math.floor(new Date().getTime() / 1000);
+	return "test_" + timestamp + "@mail.com";
+}
 
-	it("Test d'inscription réussie", () => {
-		cy.visit("https://preprod.backmarket.fr/register");
-		cy.get("#email").type(validEmail);
-		cy.get("#password").type(validPassword);
-		cy.get("#submit-signup").click();
-		cy.url().should("include", "/dashboard/orders");
+describe("Back Market Login", function () {
+	before(function () {
+		// Generate a unique email and password
+		const uniqueEmail = generateUniqueEmail();
+		const password = "Azerty12345*";
+		// Store them as global variables
+		Cypress.env("uniqueEmail", uniqueEmail);
+		Cypress.env("password", password);
 	});
 
-	it("Test d'inscription échouée", () => {
-		cy.visit("https://preprod.backmarket.fr/register");
-		cy.get("#email").type(validEmail);
-		cy.get("#password").type(validPassword);
-		cy.get("#submit-signup").click();
-		cy.get(".bg-danger-light").should("be.visible");
+	it("Create an account to Back Market", function () {
+		// Visitez la page de connexion
+		cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+		cy.get('[data-qa="accept-cta"]').click();
+		// Saisissez l'email et le mot de passe dans les champs appropriés
+		// Remplacez 'your_email' et 'your_password' par vos identifiants de connexion
+		// Remplacez également 'email_locator' et 'password_locator' par les bons selecteurs
+		cy.get("#firstName").type("yellow");
+		cy.get("#lastName").type("team");
+		const uniqueEmail = Cypress.env("uniqueEmail");
+		const password = Cypress.env("password");
+		cy.get("#signup-email").type(uniqueEmail);
+		cy.get("#signup-password").type(password);
+		console.log(uniqueEmail);
+
+		cy.get("._2OVE0h6V").click();
+		cy.get("._1UsvLiQk").click();
+		// Cliquez sur le bouton de connexion
+		// Remplacez 'submit_locator' par le bon selecteur
+		cy.get('[data-qa="signup-submit-button"]').click();
+
+		// Vérifiez que vous êtes connecté en vérifiant un élément de la page de profil
+		// Remplacez 'profile_element_locator' par le bon selecteur
+		cy.contains("Adopter un produit");
 	});
 
-	it("Test d'authentification réussie", () => {
-		cy.visit("https://preprod.backmarket.fr/register");
-		cy.get("#email").type(validEmail);
-		cy.get("#password").type(validPassword);
-		cy.get("#submit-login").click();
-		cy.url().should("include", "/dashboard/orders");
-	});
+	it("Logs in to Back Market", function () {
+		// Visitez la page de connexion
+		cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+		cy.get('[data-qa="accept-cta"]').click();
+		// Saisissez l'email et le mot de passe dans les champs appropriés
+		// Remplacez 'your_email' et 'your_password' par vos identifiants de connexion
+		// Remplacez également 'email_locator' et 'password_locator' par les bons selecteurs
+		const uniqueEmail = Cypress.env("uniqueEmail");
+		const password = Cypress.env("password");
+		cy.get("#signin-email").type(uniqueEmail);
+		cy.get("#signin-password").type(password);
 
-	it("Test d'authentification échouée", () => {
-		cy.visit("https://preprod.backmarket.fr/register");
-		cy.get("#email").type("email_incorrect@example.com");
-		cy.get("#password").type("mot_de_passe_incorrect");
-		cy.get("#submit-login").click();
-		cy.get(".bg-danger-light").should("be.visible");
+		console.log(uniqueEmail);
+		// Cliquez sur le bouton de connexion
+		// Remplacez 'submit_locator' par le bon selecteur
+		cy.get('[data-qa="signin-submit-button"]').click();
+
+		// Vérifiez que vous êtes connecté en vérifiant un élément de la page de profil
+		// Remplacez 'profile_element_locator' par le bon selecteur
+		cy.contains("Adopter un produit");
 	});
 });
